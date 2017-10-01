acf05292e4365db22a2d9f7a22f817a5bddef383
Federico Garcia Brion <fedeagb@gmail.com>
2017-10-01 14:57:10 -0300

    Parsing done. Now needs to evaluate and do tests. Maybe a refactor to make things look better.

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,7 +1,22 @@
 var Interpreter = function () {
 
-    this.parseDB = function (params, paramss, paramsss) {
-
+    this.parseDB = function (inputDB) {
+        var result = {'error': false};
+        var line = 1;
+        inputDB.forEach(function(element) {
+            var parsedElement = element.replace(/(\t|\n|\.)/g, '');
+            if(isValidFact(parsedElement)) {
+                // Process fact
+                processValidFact(parsedElement);
+            } else if(isValidRule(parsedElement)) {
+                // Process rule
+                processValidRule(parsedElement);
+            } else {
+                result = {'error': true, 'element': parsedElement, 'line': line};
+            }
+            line++;
+        });
+        return result;
     }
 
     this.checkQuery = function (params) {
@@ -10,4 +25,72 @@ var Interpreter = function () {
 
 }
 
+isValidFact = function(inputElement) {
+    var matches = inputElement.match(/^[^\(]*\([^)]*\)$/g);
+    if(matches === null) {
+        return false;
+    }
+    return true;
+};
+
+isValidRule = function(inputElement) {
+    var matches = inputElement.match(/^[^\(]*\([^)]*\) :- ([^\(]*\([^)]*\), *)*([^\(]*\([^)]*\))$/g);
+    if(matches === null) {
+        return false;
+    }
+    return true;
+};
+
+putTogether = function(value) {
+    return value.replace(/( *\( *| *\) *| *, *)/g, '');
+}
+
+processValidFact = function(fact) {
+    var together = putTogether(fact);
+    factMap.set(together, 1);
+}
+
+processValidRule = function(rule) {
+    var parsedRule = rule.replace(/ +/g, '');
+    var separated = parsedRule.split(/:-/);
+    var ruleSide = separated[0];
+    var factSide = separated[1];
+    var ruleName = ruleSide.split(/\(/)[0];
+    var ruleList = generateRuleList(ruleSide, factSide);
+    ruleMap.set(ruleName, ruleList);
+}
+
+generateRuleList = function (ruleSide, factSide) {
+    var variables = ruleSide.split(/\(/)[1].replace(/\)/g, '').split(/,/g);
+    var allFacts = factSide.split(/\) *,/g);
+    var processedFacts = allFacts.map(putTogether);
+    var ruleList = [];
+    ruleList.push(variables.length);
+    ruleList = ruleList.concat(variables);
+    ruleList = ruleList.concat(processedFacts);
+    return ruleList;
+}
+
 module.exports = Interpreter;
+
+var db = [
+    "varon(juan).",
+    "varon(pepe).",
+    "varon(hector).",
+    "varon(roberto).",
+    "varon(alejandro).",
+    "mujer(maria).",
+    "mujer(cecilia).",
+    "padre(juan, pepe).",
+    "padre(juan, pepa).",
+    "padre(hector, maria).",
+    "padre(roberto, alejandro).",
+    "padre(roberto, cecilia).",
+    "hijo(X, Y) :- varon(X), padre(Y, X).",
+    "hija(X, Y) :- mujer(X), padre(Y, X)."
+];
+
+var factMap = new Map();
+var ruleMap = new Map();
+var inter = new Interpreter();
+ inter.parseDB(db);
